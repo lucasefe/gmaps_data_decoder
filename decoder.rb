c86c620fec6322db67982f1c0456299092e1afb2
Lucas Florio <lucasefe@gmail.com>
2016-06-24 22:44:01 -0700

    Basic functionality in.

---
+++ decoder.rb
@@ -0,0 +1,41 @@
+module Gdata
+  class InvalidFormatError < Exception; end
+
+  MATRIX_MATCHER = /\d*m\d*/
+
+  module_function
+
+  def decode(data)
+    raise InvalidFormatError if data[0] != '!'
+
+    _, tail = data[1..-1].split("!", 2)
+    elements = tail.split("!")
+
+    decode_value([], elements)
+  end
+
+  def decode_value(data, elements)
+    elem = elements.shift
+
+    return unless elem
+
+    if MATRIX_MATCHER.match(elem)
+      data.push decode_value([], elements)
+    else
+      data.push parse_value(elem)
+      decode_value(data, elements)
+    end
+
+    data.compact
+  end
+
+  def parse_value(elem)
+    case elem[1]
+    when "b" then
+      elem[2] == "1"
+    else
+      elem[2..-1]
+    end
+  end
+end
+
---
+++ decoder_test.rb
@@ -0,0 +1,37 @@
+require "minitest/autorun"
+require_relative 'decoder'
+
+class TestDecoder < Minitest::Test
+  def test_fail_if_no_bang
+    assert_raises(Gdata::InvalidFormatError) { Gdata.decode("") }
+  end
+
+  def test_decode_4m5
+    data = "!4m5!3m4!1s0x808f7e2378c6037d:0xdd294d82eb235474!8m2!3d37.7649179!4d-122.4189024"
+    assert_equal [
+      [
+        "0x808f7e2378c6037d:0xdd294d82eb235474",
+        [
+          "37.7649179",
+          "-122.4189024"
+        ]
+      ]
+    ], Gdata.decode(data)
+  end
+
+  def test_decode_3m1
+    data = "!3m1!4b1!4m5!3m4!1s0x808f7e2378c6037d:0xdd294d82eb235474!8m2!3d37.7649179!4d-122.4189024"
+    assert_equal [
+      true,
+      [
+        [
+          "0x808f7e2378c6037d:0xdd294d82eb235474",
+          [
+            "37.7649179",
+            "-122.4189024"
+          ]
+        ]
+      ]
+    ], Gdata.decode(data)
+  end
+end
